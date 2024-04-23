efb3ec865d35e91fffa41f5358c0bc59dceb2d07
FabianZaz <167519433+FabianZaz@users.noreply.github.com>
2024-04-23 16:38:06 +0300

    Add files via upload

---
+++ algo.py
@@ -0,0 +1,68 @@
+#Алгоритм
+#Задача 6
+# S = 1
+# for N in range(1, 6):
+#     S *= N
+# print(f"Переменная S={S}")
+
+#Задача 3
+
+# a, s, p = 1, 0, 0
+# while True:
+#     if a > 10:
+#      break
+#     a += 2
+#     p += a
+#     s += p
+# print(f'Переменная s = {s}')
+
+#Задача 1
+# a, s = 3, 3
+# while 1:
+#     c = a + s
+#     if c == 0:
+#       break
+#     a -= 2
+#     s += 1
+# print(f'Переменная S = {s}')
+
+#Задача 2
+# a = 5
+# b = 5
+# while 0:
+#     if b > 0:
+#        b -= 1
+#        a *=2 + 1
+# print (f'Переменная A = {a}')
+
+#Задача 4
+# for x in range(5,-6,-1):
+#     y = 2 * x + 1
+#     print("End" if y < 1 else "Continue")
+
+#Задача 5
+# for x in range(5,-16,-2):
+#     for y in range(5,16,1):
+#          print("End" if x + y > 0 else "Continue")
+
+#Задача 7
+# for i in range(1,11,1):
+#     k = 10 - i
+#     for k in range(1,11,1):
+#         l = 6 - i
+#         print("End" if l < 0  else "Continue")
+
+#Задача 8
+# m, n = 12, 5
+# while 1:
+#     if m == n:
+#       break
+#     if m > n:
+#         m -= 2*n
+#     if m < n:
+#         n -= 3
+# print(f'Переменная m = {m}')
+
+
+
+
