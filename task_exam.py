7fc07ca3f665567e81d1770a14871a5d8294bc7e
FabianZaz <167519433+FabianZaz@users.noreply.github.com>
2024-05-30 10:44:45 +0300

    Add files via upload

---
+++ task_exam.py
@@ -0,0 +1,62 @@
+# a,s,p = 1,150,200
+# while True:
+#     if a>10:
+#         break
+#     a +=2
+#     p +=a
+#     s +=p
+# print(f'Конец алгоритма S={s}')
+
+# s = 1
+# for n in range (1,6):
+#     s *=n
+# print(f'Конец алгоритма S={n}')
+
+# m,n = 12,5
+# while m!=n:
+#     if m>n:
+#         m -=2*n
+#     else:
+#         n -=3
+# print(f'Значение m:{m}, n:{n}')
+
+# k,l = list(range(1,11)), list (range(1,11))
+#
+# for i in range (1, 11):
+#  k[i - 1] = 10 - i
+# for i in range (1,11):
+#   l[i - 1] = k[5] - k[i -1]
+# print(k)
+# print(l)
+# print(f'Количество отрицательных элементов массива l = {len([value for value in l if value <0])}')
+
+# c,d = 750,90
+# while True:
+#     if d>0:
+#         break
+#     d -=10
+#     c = c/2 + 50
+# print(f'Конец алгоритма C={c}')
+#
+# k, l = list(range(1,11)), list (range(1,11))
+#
+# for i in range (1,11):
+#  k [i - 1] = 10 - i
+# for i in range (1,11):
+#  l[i - 1] = k[5] - k [i -1]
+# print(k)
+# print(l)
+# count_k = 0
+# for value in k:
+#  if value < 0:
+#   count_k += 1
+#  count_l = 0
+#  for value in l:
+#   if value < 0:
+#    count_l += 1
+# print(f'Количество отрицательых элементов = {count_k + count_l}')
+#
+# count_k = len ([value for value in k if value < 0])
+# count_l = len ([value for value in l if value < 0])
+# print(f'Количество отрицательных элементов = {count_k + count_l}')
+
